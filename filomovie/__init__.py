3a83db98232c4fca6c7643452439c357a2176a10
ngc1514 <aidenx58@gmail.com>
2021-09-16 08:43:04 -0400

    database setup

--- filomovie/__init__.py
+++ filomovie/__init__.py
@@ -1,10 +1,32 @@
 import os
 from flask import Flask
 
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+'''
+NOTE: database setup boilerplate code. modify to your needs 
+''' 
+
+# create db object
+db = SQLAlchemy()
+migrate = Migrate()
+
 def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
-        SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key'
+        SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(app.instance_path, 'task_list.sqlite'),
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
     )
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from . import models
 
     return app
