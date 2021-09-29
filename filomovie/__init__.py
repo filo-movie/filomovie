9f3221d66472389dcb044832165c5895fb433fc8
trevor-schnegg <trevor.schneggenburger@gmail.com>
2021-09-29 15:16:17 -0400

    #26 Added initial database connection code in filomovie.

--- filomovie/__init__.py
+++ filomovie/__init__.py
@@ -7,9 +7,6 @@ from flask_migrate import Migrate
 NOTE: database setup boilerplate code. modify to your needs 
 ''' 
 
-# create db object
-# db = SQLAlchemy()
-# migrate = Migrate()
 
 # initialize app and database connection
 def create_app():
@@ -17,18 +14,16 @@ def create_app():
                 static_folder= 'static',
                 template_folder='static/templates')
 
-    # TODO: setup database connection here?
-    # app.config.from_mapping(
-    #     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
-    #     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #         'sqlite:///' + os.path.join(app.instance_path, 'task_list.sqlite'),
-    #     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # )
+    app.config.from_mapping(
+        SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL'),
+        SQLALCHEMY_TRACK_MODIFICATIONS = False,
+        TESTING = True
+    )
 
-    # db.init_app(app)
-    # migrate.init_app(app, db)
     return app
 
 
 app = create_app()
 
+db = SQLAlchemy(app)
