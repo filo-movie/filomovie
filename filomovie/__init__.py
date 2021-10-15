addd1977722b48e4fae8de048e0f9807aa881012
trevor-schnegg <64709466+trevor-schnegg@users.noreply.github.com>
2021-10-14 21:55:37 -0400

    Fixing a merge error. Not associated with any user story or task.

--- filomovie/__init__.py
+++ filomovie/__init__.py
@@ -1,39 +1,29 @@
+from operator import truediv
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import filomovie.database
 
-from filomovie import database
-
-'''
-NOTE: database setup boilerplate code. modify to your needs
-'''
-
-# create db object
-# db = SQLAlchemy()
-# migrate = Migrate()
 
 # initialize app and database connection
 def create_app():
+    print("[*] Creating App....", flush=True) 
     app = Flask(__name__,
                 static_folder= 'static',
                 template_folder='static/templates')
 
-    # TODO: setup database connection here?
-    # app.config.from_mapping(
-    #     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
-    #     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #         'sqlite:///' + os.path.join(app.instance_path, 'task_list.sqlite'),
-    #     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # )
-
-    # db.init_app(app)
-    # migrate.init_app(app, db)
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get("SECRET_KEY") or 'dev_key',
+        SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL").replace("postgres", "postgresql"),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        TESTING=True
+    )
+    
     return app
 
 
 app = create_app()
-db, schema_dictionary = database.create_database(app)
+db, relation_dictionary = database.create_database(app)
 if app.config.get("TESTING"):
-    database.test_conn(db, schema_dictionary["Integer"])
+    database.test_conn(db, relation_dictionary["Integer"])
 migrate = Migrate(app, db)
