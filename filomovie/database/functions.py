0bffffb7c2f3457c896af9f2612211920e41a2b1
trevor-schnegg <64709466+trevor-schnegg@users.noreply.github.com>
2021-10-14 23:25:56 -0400

    #40 Added the testing file for the database functions.

--- filomovie/database/functions.py
+++ filomovie/database/functions.py
@@ -3,7 +3,7 @@
 # Adds the data to the database if it doesn't exist already and returns True.
 # If the record already exists, will print to the screen and return False.
 def insert_movie(db, Movie, id, image, title, description, streaming_services):
-    if Movie.query.filter_by(id = id).all():
+    if Movie.query.filter_by(id=id).all():
         print("Movie already present in the database")
         return False
     else:
@@ -15,8 +15,9 @@ def insert_movie(db, Movie, id, image, title, description, streaming_services):
 # Searches the title in the database and returns a list of all records that match.
 # If no records exist, returns an empty list (will act as False if called in logic).
 def search_title(Movie, title):
-    movies_found = Movie.query.filter_by(title = title).all()
+    movies_found = Movie.query.filter_by(title=title).all()
     if movies_found:
         return movies_found
     else:
         return []
+
