ef663e94dee42a96da46ad4e068c40b871135eb6
trevor-schnegg <64709466+trevor-schnegg@users.noreply.github.com>
2021-10-18 18:42:32 -0400

    #45 Added an example of how to add (and delete) records from the database. Changed print statements to better reflect the output of the database functions.

--- filomovie/database/base_functions.py
+++ filomovie/database/base_functions.py
@@ -4,7 +4,7 @@
 # If the record already exists, will print to the screen and return False.
 def insert_movie(db, Movie, id, image, title, description, streaming_services):
     if Movie.query.filter_by(id=id).all():
-        print("Movie already present in the database")
+        print("Movie '" + str(Movie.query.filter_by(id=id).all()[0].title) + "', id=" + str(id) + " already present in the database")
         return False
     elif type(id) != int or type(image) != str or type(title) != str or type(description) != str or type(streaming_services) != str:
         print("One or more of the input values was of the incorrect type.")
@@ -12,7 +12,7 @@ def insert_movie(db, Movie, id, image, title, description, streaming_services):
     else:
         db.session.add(Movie(id, image, title, description, streaming_services))
         db.session.commit()
-        print("The following record was added: " + str(Movie.query.filter_by(id=id).all()))
+        print("The following movie was added: " + str(Movie.query.filter_by(id=id).all()[0].title) + ", id=" + str(id))
         return True
 
 # Searches the title in the database and returns a list of all records that match.
@@ -20,6 +20,10 @@ def insert_movie(db, Movie, id, image, title, description, streaming_services):
 def search_title(Movie, title):
     movies_found = Movie.query.filter_by(title=title).all()
     if movies_found:
+        print("Search results for the movie: " + title)
+        for movie in movies_found:
+            print(f"id={movie.id}, image={movie.image}, title={movie.title}, description={movie.description}, streaming_services={movie.streaming_services}")
+        print()
         return movies_found
     else:
         return []
--- filomovie/database/functions.py
+++ filomovie/database/functions.py
@@ -1,9 +1,29 @@
 from filomovie import db, relation_dictionary
 from filomovie.database import base_functions
 
+# Inserts a Movie tuple into the database with the given variables. ID MUST BE UNIQUE PER MOVIE.
 def insert_movie(id, image, title, description, streaming_services):
-    base_functions.insert_movie(db, relation_dictionary["Movie"], id, image, title, description, streaming_services)
+    return base_functions.insert_movie(db, relation_dictionary["Movie"], id, image, title, description, streaming_services)
 
+# Returns a list of Movie objects that match the search.
 def search_title(title):
-    base_functions.search_title(relation_dictionary["Movie"], title)
+    return base_functions.search_title(relation_dictionary["Movie"], title)
+
+
+# SAMPLE INSERT AND DELETE
+'''
+insert_movie(-1, "Some image", "Shrek", "A movie about an ogre and a donkey.", "Available on some streaming services")
+
+# Even though the result should only be one value, it is still in a list.
+Shrek = search_title("Shrek")[0]
+
+# Example of how to access the values
+print(str(Shrek.id) + ", " + Shrek.image + ", " + Shrek.title + ", " + Shrek.description + ", " + Shrek.streaming_services)
+
+# Deletes the value. db.session.delete takes a movie object and removes that tuple from the table.
+db.session.delete(Shrek)
+
+# Have to commit changes if you are manually deleting a record, the insert function commits for you.
+db.session.commit()
+'''
 
