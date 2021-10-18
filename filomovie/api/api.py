e120a6dd64c2840498e1c539345ff0a8109f46f5
Alex <azw745@gmail.com>
2021-10-18 15:31:58 -0400

    #37,#43 parsed the api for the necessary data and put all the data into JSON for database insertion

--- filomovie/api/api.py
+++ filomovie/api/api.py
@@ -2,17 +2,33 @@ from urllib.request import urlopen
 import json
 
 apikey = 'api_key_here'
-baseurl = 'https://api.themoviedb.org/3/'
-url1 = baseurl + 'search/movie?api_key=' + apikey + '&query=Shrek'
+baseurl = 'https://api.themoviedb.org/3/movie/'
+posterpath = 'https://image.tmdb.org/t/p/original'
 
-response1 = urlopen(url1)
-data1 = json.loads(response1.read())
-
-movieid = data1['results'][0]['id']
-url2 = 'https://api.themoviedb.org/3/movie/' + str(movieid) + '/watch/providers?api_key=' + apikey
-response2 = urlopen(url2)
-data2 = json.loads(response2.read())
-
-providers = data2['results']['US']['flatrate']
-
-print(providers)
+info_dict = {}
+stream = []
+open("data2send.txt", "w").close()
+#movie_id, movie_image, movie_title, movie_desc, stream
+with open('file_test.txt') as f:
+    for line in f:
+        data = json.loads(line)
+        movie_id = data['id']
+        info_dict['movie_id'] = movie_id
+        url = baseurl + str(movie_id) + '?api_key=' + apikey
+        output = urlopen(url)
+        info = json.loads(output.read())
+        info_dict['movie_title'] = info['title']
+        info_dict['movie_image'] = posterpath + info['poster_path']
+        info_dict['movie_desc'] = info['overview']
+        print(info)
+        url = baseurl + str(movie_id) + '/watch/providers?api_key=' + apikey
+        output = urlopen(url)
+        info = json.loads(output.read())
+        providers = info['results']['US']['flatrate']
+        for p in providers:
+            stream.append(p['provider_name'])
+        info_dict['stream_providers'] = stream
+        dump = json.dumps(info_dict)
+        g = open("data2send.txt", "a")
+        g.write(dump + '\n')
+g.close()
