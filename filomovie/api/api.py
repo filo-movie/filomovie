a4626d319c6edc4819fc03e5a0fdb0078d30ab9a
Alex <azw745@gmail.com>
2021-11-01 17:21:04 -0400

    #54 data2send will only contain english movies now and the data export has been added

--- filomovie/api/api.py
+++ filomovie/api/api.py
@@ -1,6 +1,12 @@
 import json
 from filomovie.database.functions import insert_movie
 
+def check_en(dict_list):
+    for elem in dict_list:
+        if elem['iso_639_1'] == 'en':
+            return True
+    return False
+
 def api_parse():
     from urllib.request import urlopen
     import json
@@ -10,9 +16,9 @@ def api_parse():
 
     open("data2send.txt", "w").close()
     #movie_id, movie_image, movie_title, movie_desc, stream
-    with open('file_test.txt') as f:
+    with open('movie_ids_10_18_2021.json', encoding='utf8') as f:
         for line in f:
-            info_dict = {}
+            info_dict = {'movie_id':"", 'movie_image':"", 'movie_title':"", 'movie_desc':"", 'stream_providers':""}
             stream = []
             providers = []
             data = json.loads(line)
@@ -21,23 +27,26 @@ def api_parse():
             url = baseurl + str(movie_id) + '?api_key=' + apikey
             output = urlopen(url)
             info = json.loads(output.read())
-            info_dict['movie_title'] = info['title']
-            info_dict['movie_image'] = posterpath + info['poster_path']
-            info_dict['movie_desc'] = info['overview']
-            url = baseurl + str(movie_id) + '/watch/providers?api_key=' + apikey
-            output = urlopen(url)
-            info = json.loads(output.read())
-            if 'flatrate' in info['results']['US']:
-                providers = info['results']['US']['flatrate']
-                for p in providers:
-                    stream.append(p['provider_name'])
-            else:
-                stream = []
-            info_dict['stream_providers'] = stream
-            dump = json.dumps(info_dict)
-            g = open("data2send.txt", "a")
-            g.write(dump + '\n')
-            g.close()
+            if info['original_language'] == 'en' and check_en(info['spoken_languages']):
+                info_dict['movie_title'] = info['title']
+                if info['poster_path'] != None:
+                    info_dict['movie_image'] = posterpath + info['poster_path']
+                info_dict['movie_desc'] = info['overview']
+                url = baseurl + str(movie_id) + '/watch/providers?api_key=' + apikey
+                output = urlopen(url)
+                info = json.loads(output.read())
+                if info['results'] and 'US' in info['results'].keys():
+                    if 'flatrate' in info['results']['US']:
+                        providers = info['results']['US']['flatrate']
+                        for p in providers:
+                            stream.append(p['provider_name'])
+                    else:
+                        stream = []
+                info_dict['stream_providers'] = stream
+                dump = json.dumps(info_dict)
+                g = open("data2send.txt", "a", encoding='utf8')
+                g.write(dump + '\n')
+                g.close()
 
 api_parse()
 with open("data2send.txt") as f:
