c983a001d3f3887a32a8216a1ad6ba2c2a262302
Afee Daiyan <afeedaiy@buffalo.edu>
2021-11-28 18:40:46 -0500

    #79 this commit adds the prefix for safari to the css to fix the misaligned go button

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def movie_details():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port, debug=True)
