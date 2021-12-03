045545d9226b636a028dbb3146d9452c66a61eeb
ngc1514 <aidenx58@gmail.com>
2021-12-02 22:53:04 -0500

    #81 Added rating,runtime,release_date,genres processing in backend.py

--- filomovie/backend.py
+++ filomovie/backend.py
@@ -25,6 +25,13 @@ def process_search(searchedMovie):
         # NOTE: escaping double quotes cuz they fucked up json
         curObj['movie_desc'] = movie.description.replace('"', '\\"')
         curObj['streaming_services'] = movie.streaming_services.replace('"', '\\"')
+        curObj['movie_runtime'] = movie.runtime
+        curObj['movie_rating'] = movie.rating
+        curObj['movie_release_date'] = movie.release_date
+
+        # FIXME: fix after merging with dev
+        curObj['movie_genres'] = movie.genres
+
         movieJsonObj['moviesFound'].append(curObj)
     # print("size of result: " + str(len(movieJsonObj['moviesFound'])), flush=True)
 
@@ -39,9 +46,17 @@ def process_detail(postedJson):
     tempDict['movie_title'] = postedJson['movie_title']
     tempDict['movie_image'] = postedJson['movie_image']
     tempDict['movie_desc'] = postedJson['movie_desc'].replace('"', "\'")
-    
+
+    tempDict['movie_runtime'] = postedJson['movie_runtime']
+    tempDict['movie_rating'] = postedJson['movie_rating']
+    tempDict['movie_release_date'] = postedJson['movie_release_date']
+
     tempDict['streaming_services'] = []
     for service in json.loads(postedJson['streaming_services']):
         tempDict['streaming_services'].append(service)
+
+    tempDict['movie_genres'] = []
+    for genre in json.loads(postedJson['movie_genres']):
+        tempDict['movie_genres'].append(genre)
 
     return tempDict
